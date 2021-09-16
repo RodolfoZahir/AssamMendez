fd985f0bfd73ecfbf817b8ed0916b5a789b77d0a
RodolfoZahir <zahirvlogs121101@gmail.com>
2021-09-16 13:54:38 -0500

    Nuevos Cambios

--- ProyectosJava/Tarea 1 Serie Numeros (Do-while)/src/App.java
+++ ProyectosJava/Tarea 1 Serie Numeros (Do-while)/src/App.java
@@ -1,7 +1,7 @@
 public class App {
     public static void main(String[] args) throws Exception {
         int NumInicial1 = 1, NumInicial2 = 2, Suma = 0, Limite = 10, Contador = 0;
-        do 
+        while(Contador <= Limite)
         {
             Suma = NumInicial1 + NumInicial2;
             System.out.println("Resultado: " + Suma);
@@ -9,6 +9,5 @@ public class App {
             NumInicial1 = NumInicial2;
             NumInicial2 = Suma;
         }
-        while(Contador <= Limite);
     }
 }
